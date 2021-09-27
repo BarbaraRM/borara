123688db7db13528ca0bfcdd900a29d0722d5909
BarbaraRM <bromeromachare@gmail.com>
2021-09-27 01:15:11 -0500

    first commit

---
+++ config/properties.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: process.env.PORT || 3000,
+  //DB: 'mongodb://localhost:27017/loginMean'
+  DB: 'mongodb+srv://barbara:juandavid@clustercartera.lksyk.mongodb.net/ProgramaCartera?retryWrites=true&w=majority'
+}
